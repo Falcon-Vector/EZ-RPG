02f98833bc715775a4a6554c47d28a0d704784df
Falcon-Vector <falconx31.777@gmail.com>
2026-05-30 22:44:24 +0530

    Main player attack+movement and enemy done

---
+++ enemy.gd
@@ -0,0 +1,80 @@
+extends CharacterBody2D
+
+var speed = 50
+var player = null
+var player_chase = false
+
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
+
+func _physics_process(delta):
+	deal_with_damage()
+
+	velocity = Vector2.ZERO
+
+	if player_chase and player:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+
+		$AnimatedSprite2D.play("walk")
+
+		if direction.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+
+	else:
+		$AnimatedSprite2D.play("idle")
+
+	move_and_slide()
+
+
+func _on_detection_area_body_entered(body):
+	if body.name == "player":
+		player = body
+		player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	if body == player:
+		player = null
+		player_chase = false
+		
+func enemy():
+	pass 
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	print("Entered:", body.name)
+
+	if body.has_method("player"):
+		player_inattack_zone = true
+		print("Player in attack zone")
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_with_damage():
+	print(
+		"in_zone=", player_inattack_zone,
+		" attacking=", global.player_current_attack,
+		" can_take=", can_take_damage
+	)
+
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cooldown_timeout() -> void:
+	can_take_damage = true
+	print("Cooldown reset")
+	
---
+++ scripts/player.gd
@@ -0,0 +1,147 @@
+extends CharacterBody2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var attack_ip = false
+
+const speed = 100
+var current_dir = "down"
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	if not global.player_current_attack:
+		player_movement(delta)
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		print("you have died!")
+		self.queue_free()
+		
+func player_movement(_delta):
+		if Input.is_action_pressed("d"):
+			current_dir = "right"
+			play_anim(1)
+			velocity.x = speed
+			velocity.y = 0
+		elif Input.is_action_pressed("a"):
+			current_dir = "left"
+			play_anim(1)
+			velocity.x = -speed
+			velocity.y = 0
+		elif Input.is_action_pressed("s"):
+			current_dir = "down"
+			play_anim(1)
+			velocity.y = speed
+			velocity.x = 0
+		elif Input.is_action_pressed("w"):
+			current_dir = "up"
+			play_anim(1)
+			velocity.y = -speed
+			velocity.x = 0
+		else:
+			play_anim(0)
+			velocity.x = 0
+			velocity.y = 0
+			
+		move_and_slide()
+		
+func play_anim(movement):
+	var dir = current_dir
+	var anim = sprite
+	
+
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		else:
+			if attack_ip == false:
+				anim.play("side_idle")
+
+	elif dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		else:
+			if attack_ip == false:
+				anim.play("side_idle")
+
+	elif dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("front_walk")
+		else:
+			if attack_ip == false:
+				anim.play("front_idle")
+
+	elif dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("back_walk")
+		else:
+			if attack_ip == false:
+				anim.play("back_idle")
+
+func player():
+	pass
+
+
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_player_hitbox_area_entered(area: Area2D) -> void:
+	
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_area_exited(area: Area2D) -> void:
+	
+		enemy_inattack_range = false
+
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			sprite.flip_h = false
+			sprite.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			sprite.flip_h = true
+			sprite.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			sprite.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			sprite.play("back_attack")
+			$deal_attack_timer.start()
+			
+
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
+	
